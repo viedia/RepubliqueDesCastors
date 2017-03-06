0449cb322bd3280ea4a0207b91a9b53fee81f303
viedia <laetitia.lemiere@gmail.com>
2017-03-06 12:54:17 +0100

    correction de la css

---
+++ js/affichageVille.js
@@ -0,0 +1,16 @@
+"use strict";
+
+
+var cy = cytoscape({
+container: document.getElementById('cy'),
+        elements: [
+        { data: { id: 'a' } },
+        { data: { id: 'b' } },
+        {
+            data: {
+             id: 'ab',
+             source: 'a',
+               target: 'b'
+          }
+    }]
+});
